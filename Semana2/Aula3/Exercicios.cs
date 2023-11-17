7adf4463ea2ab8809d436de799631c54299e9cb3
DStalkerBR <jhon89araujo@gmail.com>
2023-11-16 21:13:43 -0300

    Soluciona exercicios da aula 3 e 4

---
+++ Semana2/Aula3/Exercicios.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region Exemplo de Laço de Repetição: Foreach
+string [] colecao = {"Item1", "Item2", "Item3", "Item4"};
+
+foreach (var item in colecao)
+{
+    Console.WriteLine(item);
+}
+
+#endregion
+
+// Escreva um programa em C# que imprime todos os números que são
+// divisíveis por 3 ou por 4 entre 0 e 30;
+#region  Números divisíveis
+Console.WriteLine("Números divisíveis por 3 ou por 4: ");
+    for (int num = 0; num <= 30; num++){
+        if (num % 3 == 0 || num % 4 == 0){            
+            Console.WriteLine(num);
+        }
+    }
+#endregion
+
+// Faça um programa em C# que imprima os primeiros números da série de
+// Fibonacci até passar de 100. A série de Fibonacci é a seguinte: 0, 1, 1, 2, 3,
+// 5, 8, 13, 21 etc... Para calculá-la, o primeiro elemento vale 0, o segundo vale
+// 1, daí por diante, o n-ésimo elemento vale o (n-1)-ésimo elemento somado
+// ao (n-2)-ésimo elemento (ex: 8 = 5 + 3)
+#region Sequência Fibonacci
+    int fib1 = 0;
+    int fib2 = 1;
+    int fib3 = 0;
+    Console.WriteLine("================================");
+    Console.WriteLine("       Sequência Fibonacci      ");
+    Console.WriteLine("================================");
+    Console.WriteLine(fib1);
+    Console.WriteLine(fib2);
+    while (fib3 <= 100){
+        fib3 = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib3;
+        Console.WriteLine(fib3);
+    }
+#endregion
+
+// Faça um programa que
+// imprima a seguinte tabela
+// até o nível 8:
+#region Tabela de multiplicação
+int n = 8;
+Console.WriteLine("================================");
+Console.WriteLine("       Tabela de multiplicação      ");
+Console.WriteLine("================================");
+for (int i = 1; i <= n; i++){
+    for (int j = 1; j <= i; j++){
+        Console.Write(" " + j * i + " ");
+    }
+    Console.WriteLine();
+}
+#endregion
---
+++ Semana2/Aula4_Exercicios/Exercicios.cs
@@ -0,0 +1,67 @@
+﻿// Suponha que você tenha uma string contendo uma data no formato
+// "dd/mm/aaaa" (por exemplo, "25/10/2023"). Crie um programa que divida a
+// string em dia, mês e ano e exiba cada parte separadamente.
+
+#region  Separando Data
+    string data = "25/10/2023";
+
+    string[] dataArray = data.Split('/');
+
+    Console.WriteLine("================================");
+    Console.WriteLine("          String de Data        ");
+    Console.WriteLine("================================");
+
+    Console.WriteLine("Dia: " + dataArray[0]);
+    Console.WriteLine("Mês: " + dataArray[1]);
+    Console.WriteLine("Ano: " + dataArray[2]);
+#endregion
+
+// Crie um programa que inicialize um array de inteiros com números de 1 a 10.
+// Em seguida, itere pelo array e exiba apenas os números pares.
+
+#region Numeros Pares
+    int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    Console.WriteLine("================================");
+    Console.WriteLine("     Numeros Pares (1..10)      ");
+    Console.WriteLine("================================");
+
+    foreach (int numero in numeros)
+    {
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+#endregion
+
+// Crie uma lista de strings contendo nomes de cidades. Adicione mais
+// algumas cidades a essa lista. Em seguida, crie um programa que itere pela
+// lista e exiba todas as cidades cujos nomes começam com a letra "S".
+
+#region
+    List<string> cidades = new List<string>()
+    {
+        "Itabuna",
+        "Ilheus",
+        "Salvador"
+    };
+    cidades.Add("Itacaré");
+    cidades.Add("Porto Seguro");
+    cidades.Add("Santarém");
+    cidades.Add("Santa Maria");
+
+    Console.WriteLine("================================");
+    Console.WriteLine("       Cidades com S          ");
+    Console.WriteLine("================================");
+
+    foreach (string cidade in cidades)
+    {
+        if (cidade.StartsWith("S"))
+        {
+            Console.WriteLine(cidade);
+        }
+    }
+
+#endregion
+
