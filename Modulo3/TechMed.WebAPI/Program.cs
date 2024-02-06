b73e5b2ffb5bc9be3d12e6a5fa01932f5d4ecb91
DStalkerBR <jhon89araujo@gmail.com>
2024-02-06 16:49:56 -0300

    Add auth to techmed project swagger

--- Modulo3/TechMed.WebAPI/Program.cs
+++ Modulo3/TechMed.WebAPI/Program.cs
@@ -1,13 +1,15 @@
+using Microsoft.OpenApi.Models;
 using TechMed.Application.Services;
 using TechMed.Application.Services.Interfaces;
 using TechMed.Infrastructure.Persistence;
 using TechMed.Infrastructure.Persistence.Interfaces;
+using TechMed.Application.Auth;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-// builder.Services.AddSingleton<ITechMedContext, TechMedContext>();
+builder.Services.AddSingleton<ITechMedContext, TechMedContext>();
 builder.Services.AddScoped<IMedicoService, MedicoService>();
 builder.Services.AddScoped<IPacienteService, PacienteService>();
 builder.Services.AddScoped<IAtendimentoService, AtendimentoService>();
@@ -18,7 +20,34 @@ builder.Services.AddDbContext<TechMedDbContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Simple_Auth.WebAPI", Version = "v1" });
+    c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "basic",
+        In = ParameterLocation.Header,
+        Description = "Basic Authorization header using the Bearer scheme."
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "basic"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
 
 var app = builder.Build();
 
@@ -34,5 +63,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseMiddleware<SimpleAuthHandler>();
 
 app.Run();
