0ffbc0cc27d7791ae51ec19d7f328e7a99177acc
DStalkerBR <jhon89araujo@gmail.com>
2024-02-27 16:36:42 -0300

    update context and migration (partial)

--- Modulo5/MvcMovie/Data/MvcMovieContext.cs
+++ Modulo5/MvcMovie/Data/MvcMovieContext.cs
@@ -17,5 +17,26 @@ namespace MvcMovie.Data
         public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
 
         public DbSet<MvcMovie.Models.User> User { get; set; } = default!;
+
+        public DbSet<MvcMovie.Models.Artist> Artist { get; set; } = default!;
+
+        public DbSet<MvcMovie.Models.Studio> Studio { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MvcMovie.Models.Movie>().ToTable("Movie");
+            modelBuilder.Entity<MvcMovie.Models.User>().ToTable("User");
+            modelBuilder.Entity<MvcMovie.Models.Artist>().ToTable("Artist");
+            modelBuilder.Entity<MvcMovie.Models.Studio>().ToTable("Studio");
+
+            modelBuilder.Entity<MvcMovie.Models.Movie>()
+                .HasOne(m => m.Studio)
+                .WithMany(s => s.Movies)
+                .HasForeignKey(m => m.StudioId);
+
+            modelBuilder.Entity<MvcMovie.Models.Movie>()
+                .HasMany(m => m.Artists)
+                .WithMany(a => a.Movies);
+        }
     }
 }
