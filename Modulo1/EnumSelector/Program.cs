78d747ea9c5a3e794a65d4ff9b341a8f5da14f84
DStalkerBR <jhon89araujo@gmail.com>
2024-03-15 20:45:08 -0300

    add exercises 2 and 3 from class 2

---
+++ Modulo1/EnumSelector/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace EnumSelector;
+
+class Program
+{
+    enum Exercicio
+    {
+        Academia = 1,
+        Luta = 2,
+        Corrida = 3
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Opções de exercícios disponíveis:");
+
+        foreach (Exercicio exercicio in Enum.GetValues(typeof(Exercicio)))
+        {
+            Console.WriteLine($"{(int)exercicio}. {exercicio}");
+        }
+
+        Console.WriteLine("Digite o número do exercício desejado:");
+        
+        string? input = Console.ReadLine();
+
+        try
+        {
+            int numeroExercicio = int.Parse(input!);
+            var exercicioEscolhido = (Exercicio)numeroExercicio;
+
+            if (!Enum.IsDefined(typeof(Exercicio), exercicioEscolhido))
+            {
+                throw new ArgumentException();
+            }
+
+            Console.WriteLine($"Exercício escolhido: {exercicioEscolhido}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erro: Digite um número válido.");
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("Erro: Digite um número correspondente a um exercício válido.");
+        }
+    }
+}
---
+++ Modulo1/Excecoes/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    try
+    {
+        object o = null;
+        o.ToString();
+    }
+    catch (NullReferenceException ex)
+    {
+        throw new Exception("Ocorreu um erro: Tentativa de realizar uma operação em um objeto nulo.", ex);
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
